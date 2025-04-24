c601732d665fdf0bb1165cabb5ec3ce6d77a66f3
juan pablo gamarra <jgamarra50@unab.edu.co>
2025-04-24 12:07:24 -0500

    se configuró el login

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,16 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', HomeController::class);
+
 
 Route::prefix('/products')->controller(ProductController::class)->group(function(){
     Route::get('/', "index");
     Route::get('/create', 'create');
     Route::get('/{id}', 'show');
 });
+
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/',[HomeController::class, 'welcome']);
